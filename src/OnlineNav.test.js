2ea8e74d2f77612c6e04210d2cb964bc2aab667d
Antonio Busa <antoniobusa@Antonios-MacBook-Pro.local>
2019-07-18 17:19:22 -0400

    fixed more unit tests

--- src/OnlineNav.test.js
+++ src/OnlineNav.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import OnlineNav from './OnlineNav';
-import BottomNavigationAction from '@material-ui/core';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -15,6 +15,14 @@ describe('OnlineNav component', () => {
     it('should assert the label of the button when loaded', () => {
         const wrapper = mount(<OnlineNav />);
         expect(wrapper.find(BottomNavigationAction).props().label).toBe('Expand Online Users');
+    });
+    it('should assert the label of the button after clicked after users are loaded', () => {
+        const wrapper = mount(<OnlineNav />);
+        const button = wrapper.find(BottomNavigationAction);
+        setTimeout(() => {
+            button.simulate('click');
+            expect(wrapper.find(BottomNavigationAction).props().label).toBe('Minimize Online Users');
+        }, 3000);        
     });
 
     // it('should assert the label of the button when loaded', () => {
